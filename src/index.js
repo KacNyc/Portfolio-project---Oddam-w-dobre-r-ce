7414dd5691016939b911eead4a48179d0c76085b
KacNyc <nyczak3@gmail.com>
2024-07-25 18:40:32 +0200

    Completed steps from level 1: #1a Main page - basic structure - working server

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
+//index.js
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './scss/main.scss';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-serviceWorker.unregister();
 
